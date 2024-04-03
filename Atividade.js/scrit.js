5a8ddd4e1cc0907b37367fb03ccd2ef9f793fe55
Akashisenjuro <126684134+akashisenjuro@users.noreply.github.com>
2024-04-03 11:39:24 -0300

    Add files via upload

---
+++ Atividade.js/scrit.js
@@ -0,0 +1,86 @@
+//01)Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba a soma desses números no console.
+let x=10;
+let y=10;
+let soma;
+
+soma = x+y;
+
+console.log (soma) 
+
+//02) Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba a subtração desses números no console.
+let x1=11;
+let y1=11;
+let subtração;
+
+subtração = x1-y1;
+
+console.log (subtração) 
+
+//03) Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba o produto desses números no console.
+let x2=14;
+let y2=14;
+let produto;
+
+produto = x2*y2;
+
+console.log (produto) 
+
+//04) Declare duas variáveis e atribua a elas valores numéricos. Em seguida, exiba a divisão desses números no console.
+let x3=14;
+let y3=14;
+let divisão;
+
+ divisão= x3/y3;
+
+console.log (divisão) 
+
+
+
+//05) Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba o quadrado desse número no console.
+let x4=14;
+let y4=14;
+let potencia;
+
+potencia= x4^y4;
+
+console.log (potencia) 
+
+
+
+//06) Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba a raiz quadrada desse número no console.
+let x5=22;
+let y5=2;
+let raiz;
+
+raiz= Math.sqrt(44);
+
+console.log (raiz) 
+
+//07) Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba o módulo desse número (valor absoluto) no console.
+let x6=22;
+let y6=2;
+let absoluto;
+
+absoluto= 9;
+
+console.log (absoluto) 
+
+//08) Declare uma variável e atribua a ela um valor numérico. Em seguida, exiba se o número é par ou ímpar no console.
+let x7=10;
+let y7=10;
+let ;
+
+soma = 7+7;
+
+console.log () 
+
+
+
+
+
+
+
+
+
+
+
